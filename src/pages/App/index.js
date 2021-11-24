c7ba14ebda1e1db4bd97a605911ec4a957a26dc7
Taivanjargal11 <s18ud1101039@nmit.edu.mn>
2021-11-24 15:53:45 +0800

    nmit admin

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -3,7 +3,7 @@ import css from "./style.module.css";
 
 // import Toolbar from "../../components/Toolbar";
 // import BurgerPage from "../BurgerPage";
-import SideBar from "../../components/SideBar";
+
 // import OrderPage from "../OrderPage";
 import { Route, Switch } from "react-router-dom";
 // import ShippingPage from "../ShippingPage";
